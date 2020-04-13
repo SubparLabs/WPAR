4b94dd3228dcd84713f4d168797cc40961728564
dvd <dvd@subpar.biz>
2020-04-12 20:52:42 -0400

    create addDJ endpoint

---
+++ src/lambda/addDj.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+
+const apiRoot = 'https://stream.subpar.fm/api/station/1/streamers';
+
+exports.handler = (event, context, callback) => {
+
+	const name = event. queryStringParameters.n || "anon";
+	
+	axios({
+		method:'post', 
+		url:apiRoot,
+		headers: {
+			'X-API-Key': process.env.SUBPAR_API_KEY
+		},
+		data: {
+			streamer_username: name,
+			streamer_password: "password",
+			display_name: name,
+			is_active: true,
+			enforce_schedule: false,
+			scheule_items: [
+				null
+			]
+		}
+	}).then(res => {
+		callback(null, {
+			statusCode: 200,
+			body: JSON.stringify(res.data)
+		});
+	})
+	.catch(err => {
+		callback(err);
+	});
+
+};
