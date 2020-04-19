a16dd1747cd8ce5a6b3e18f078b0f835cd513fbd
dvd <dvd@subpar.biz>
2020-04-19 15:29:17 -0400

    about page margin

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container">
     <h1>WPAR: Good Enough.</h1>
     <div class="footer"><p><a href="https://twitter.com/subparr">We have a twitter</a>.</p>
     <p><a href="http://subpar.biz">We have another dumb website</a>.</p>
@@ -8,7 +8,7 @@
 	    	<p> We probably won't ever send you an email, but here is a newsletter signup: <form action="https://tinyletter.com/subpar" method="post" target="popupwindow" onsubmit="window.open('https://tinyletter.com/subpar', 'popupwindow', 'scrollbars=yes,width=800,height=600');return true"><input type="text" style="width:140px" placeholder="your email" name="email" id="tlemail" /><input type="hidden" value="1" name="embed"/><input type="submit" value="Subscribe" /><a href="https://tinyletter.com" target="_blank"></a></form></p>
 	    	 
     	</div>
-      <div style='margin-top: 1rem'><router-link to='/'>go home</router-link></div>
+      <div style='margin-top: 1rem'><router-link to='/'>&#60; go home</router-link></div>
       </div>
   </div>
 </template>
@@ -138,7 +138,9 @@
     }
   }
 }
-
+.container {
+  margin: 40px;
+}
 .footer {
   padding: 25px;
   font-size: 0.7rem;
